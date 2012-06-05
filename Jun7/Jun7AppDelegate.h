d25176b94c1e13ee5448e89e47f21364de0a5a4b
Apechai <matthew.fong@gmail.com>
2012-06-04 22:58:39 -0400

    first commit

---
+++ Jun7/Jun7AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jun7AppDelegate.h
+//  Jun7
+//
+//  Created by Matthew Fong on 6/4/12.
+//  Copyright (c) 2012 Goldman Sachs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jun7AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
